9f45230ce0ba026cd8c94b3f84fff61b77c40125
oyknue <oyknue.lee@gmail.com>
2019-06-10 11:50:51 +0900

    header front

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,23 @@
 // navi-bar
 $(".navi > li").mouseover(function(){
-  $(this).find(".navi-bar").addClass("navi-bar-hover");
-  $(this).find(".navi-subs").css({"display":"flex"});
-  $(this).find(".navi-subs").stop().animate({"top":"2.5rem", "opacity":1},500);
+  switch($(this).index()){
+      case 0:
+      case 3:
+      $(this).find(".navi-subs").css({"display":"flex"});
+      break;
+      case 1:   
+      case 2:   
+      case 4:   
+      $(this).find(".navi-subs").css({"display":"block"}); 
+      break;
+      break;
+      default:
+        break;
+      }
+      $(this).find(".navi-bar").addClass("navi-bar-hover");
+      $(this).find(".navi-subs").stop().animate({"top":"2.5rem", "opacity":1},500);
 });
+
 $(".navi > li").mouseleave(function(){
   $(this).find(".navi-bar").removeClass("navi-bar-hover");
   $(this).find(".navi-subs").stop().animate({"top":"5rem", "opacity":0},500, function(){

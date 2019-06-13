3792c47642aea4c735edf197fba796746aaf018d
oyknue <oyknue.lee@gmail.com>
2019-06-13 14:14:08 +0900

    admin

--- public/js/admin.js
+++ public/js/admin.js
@@ -17,39 +17,44 @@ function includeHTML() {
 				if (this.readyState == 4) {
 					if (this.status == 200) {
 						elmnt.innerHTML = this.responseText;
-						// 상단 네비게이션
-						$(".navi > li").mouseenter(function(){
-							$(this).find(".navi-sub").css({"opacity":0, "display":"block"});
-							$(this).find(".navi-sub").stop().animate({"opacity":1, "top":"34px"}, 300);
+						//서버에서 불러온 html이 적용된 후
+						$.ajax({
+							type: "get",
+							url: "/ajax/cate/top/r",
+							dataType: "json",
+							success: function (res) {
+								var html = '';
+								html += '<ul class="navi-sub">';
+								for(var i in res) {
+									html += '<li class="link-elm" data-url="'+res[i].catelink+'">'+res[i].catename+'</li>';
+								}
+								html += '</ul>';
+								$(".navi > li").eq(1).append(html);
+								$(".navi > li").mouseenter(function(){
+									$(this).find(".navi-sub").css({"opacity":0, "display":"block"});
+									$(this).find(".navi-sub").stop().animate({"opacity":1, "top":"34px"}, 300);
+								});
+								$(".navi > li").mouseleave(function(){
+									$(this).find(".navi-sub").stop().animate({"opacity":0, "top":"80px"}, 300, function(){
+										$(this).css({"display":"none"});
+									});
+								});
+								$(".link-elm").click(function(){
+									var url = $(this).data("url");
+									var tar = $(this).data("tar");
+									if(tar == "_blank") window.open(url);
+									else location.href = url;
+								});
+							}
 						});
-						$(".navi > li").mouseleave(function(){
-							$(this).find(".navi-sub").stop().animate({"opacity":0, "top":"80px"}, 300, function(){
-								$(this).css({"display":"none"});
-							});
-            });
-            
-            $(".link-elm").click(function(){
-              var url = $(this).data("url");
-              var tar = $(this).data("tar");
-              if(tar == "_blank"){
-                window.open(url);
-              }else{
-                location.href = url;
-              }
-            });
-
-
-
 					}
 					if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-					/* Remove the attribute, and call this function once more: */
 					elmnt.removeAttribute("w3-include-html");
 					includeHTML();
 				}
 			} 
 			xhttp.open("GET", file, true);
 			xhttp.send();
-			/* Exit the function: */
 			return;
 		}
 	}
